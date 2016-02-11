8929b6fe755bf80d931001aed67e0e9806e13358
wxue <wxue.fullstack@gmail.com>
2016-02-11 08:23:04 -0500

    tweaks on dummy data -- make some differences; tweaks on style;

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -7,6 +7,8 @@ weHousingApp.controller('AptCtrl', function ($scope, $log, AptItem) {
 
   $scope.apt_list = [];
   $scope.apt_list_no_data = true;
+
+  // fetch data
   $scope.get_aptitem_data = function () {
     AptItem.get({
     }, function(data) {
@@ -21,15 +23,19 @@ weHousingApp.controller('AptCtrl', function ($scope, $log, AptItem) {
       $log.error(error);
     });
   }
+
   // initialize
   $scope.get_aptitem_data();
   $scope.sort_key = 'popularity';
   $scope.reverse = true;
+
+  // toggle sort key
   $scope.sortby = function(sort_key) {
     $scope.reverse = ($scope.sort_key === sort_key) ? !$scope.reverse : false;
     $scope.sort_key = sort_key;
   };
 
+  // synchronize data
   $scope.$watchCollection(
       function () {
         return $scope.apt_list_no_data;
